e57b5768529c26bf4f2a88550ca8a27ed6519604
kishanrakesh <kishanrakesh@gmail.com>
2020-03-27 10:24:06 +0530

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,7 +13,7 @@ import { AgmCoreModule } from '@agm/core';
     AgmCoreModule.forRoot({
       // please get your own API key here:
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
-      apiKey: 'AIzaSyDkPxZSJLcEGIHTVoQMo_mo1mkQ5lPRzgM'
+      apiKey: 'AIzaSyD-taABNz2aDYz60bx7RDi1Pkto9RP5xmA'
     })
   ],
   declarations: [ AppComponent, HelloComponent ],
